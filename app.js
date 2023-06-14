05da8a57480196383b1765f684e7fe4e240b0d9e
Cosmas-Emeka <cosmasemeka11@gmail.com>
2023-06-14 23:55:38 +0100

    Refurbished nav section, header section underway

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 var navLinks = document.getElementById("navLinks");
-var timesIcon = document.getElementById("timesIcon");
-var menuIcon = document.getElementById("menuIcon");
-var test = document.getElementById("test");
+var menuIcon = document.getElementsById("menuIcon");
 
 
 function showMenu() {
-    navLinks.style.left = "0";
-    timesIcon.style.display = "block";
-    menuIcon.style.display = "none";
+    navLinks.style.left = "0%";
 }
 
 function hideMenu() {
     navLinks.style.left = "-100%";
-    menuIcon.style.display = "block";
-    timesIcon.style.display = "none";
-}
-
-function navPopup() {
-    test.style.display = "flex";
-}
-
-
-function popupLeave() {
-    test.style.display = "none";
 }
